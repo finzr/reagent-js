cb9fd26901e55afe7b51daef98fb5275e47c7d5e
Виталий Машанов <vvmashanov@yandex.ru>
2018-01-24 13:39:51 +0300

    Добавлен content-type для fetchAddresses

--- src/Form/Fields/Fias/fetch_data.js
+++ src/Form/Fields/Fias/fetch_data.js
@@ -1,4 +1,7 @@
-export function fetchAddresses(url,  headers={}, query) {
+export function fetchAddresses(url,  headerOptions={}, query) {
+  const headers = Object.assign({
+    'Content-Type': 'application/json'
+  }, headerOptions)
   return fetch(`${url}`, {
     headers,
     credentials: 'same-origin',
